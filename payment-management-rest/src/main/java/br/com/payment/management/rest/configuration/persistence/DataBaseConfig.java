8cc9ad131f00e8cce893ca6cafbcd59e279ed785
William Pedrini Custodio <william@pedrinicustodio.com>
2017-12-24 20:07:05 +0000

    Creating initial structure for the project.

---
+++ payment-management-rest/src/main/java/br/com/payment/management/rest/configuration/persistence/DataBaseConfig.java
@@ -0,0 +1,56 @@
+package br.com.payment.management.rest.configuration.persistence;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.boot.orm.jpa.hibernate.SpringNamingStrategy;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.persistenceunit.PersistenceUnitManager;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+/**
+ * Class responsible for setting up the local database configuration.
+ *
+ * @author wcustodio.
+ */
+@Configuration
+public class DataBaseConfig implements EnvironmentAware {
+
+    @Autowired(required = false)
+    private PersistenceUnitManager persistenceUnitManager;
+
+    private RelaxedPropertyResolver jpaPropertyResolver;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.jpaPropertyResolver = new RelaxedPropertyResolver(environment, "spring.jpa.");
+    }
+
+    @Bean
+    @DependsOn("jdbcTemplate")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory( DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+
+        final LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        if (persistenceUnitManager != null) {
+            entityManagerFactoryBean.setPersistenceUnitManager(persistenceUnitManager);
+        }
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setPackagesToScan("br.com.payment.management");
+        entityManagerFactoryBean.getJpaPropertyMap().putAll(jpaPropertyResolver.getSubProperties("properties."));
+
+        final Map<String, Object> properties = entityManagerFactoryBean.getJpaPropertyMap();
+        properties.put("hibernate.ejb.naming_strategy", jpaPropertyResolver.getProperty("hibernate.naming-strategy", SpringNamingStrategy.class.getName()));
+        properties.put("hibernate.hbm2ddl.auto", jpaPropertyResolver.getProperty("hibernate.ddl-auto", "none"));
+
+        return entityManagerFactoryBean;
+    }
+}
