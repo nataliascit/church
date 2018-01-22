e8e39765217d1f97ad97029a6dac47884273f702
William Custódio <willpcustodio@gmail.com>
2018-01-22 23:18:37 +0000

    New application module.

---
+++ payment-management-rest/src/main/java/br/com/payment/management/webapp/Application.java
@@ -0,0 +1,19 @@
+package br.com.payment.management.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Class responsible for setting up the application.
+ *
+ * @author wcustodio
+ */
+@SpringBootApplication(scanBasePackages = {"br.com.payment.management.core", "br.com.payment.management.webapp"})
+@EnableJpaRepositories(basePackages = {"br.com.payment.management.core.repository"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
