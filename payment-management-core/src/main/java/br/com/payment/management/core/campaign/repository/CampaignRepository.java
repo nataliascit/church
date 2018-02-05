272afc822ac49abfc53a0064fe9d46bb645f7dad
William Custodio <william@pedrinicustodio.com>
2018-02-05 22:09:32 +0000

    [BACKEND] Initializing the configuration of the login module.

--- payment-management-core/src/main/java/br/com/payment/management/core/campaign/repository/CampaignRepository.java
+++ payment-management-core/src/main/java/br/com/payment/management/core/campaign/repository/CampaignRepository.java
@@ -2,11 +2,13 @@ package br.com.payment.management.core.campaign.repository;
 
 import br.com.payment.management.core.campaign.model.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * {@link Campaign} Data Access Object.
  *
  * @author wcustodio
  */
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
 }
--- payment-management-core/src/main/java/br/com/payment/management/core/church/repository/ChurchRepository.java
+++ payment-management-core/src/main/java/br/com/payment/management/core/church/repository/ChurchRepository.java
@@ -2,11 +2,13 @@ package br.com.payment.management.core.church.repository;
 
 import br.com.payment.management.core.church.model.Church;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * {@link Church} Data Access Object.
  *
  * @author wcustodio
  */
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface ChurchRepository extends JpaRepository<Church, Long> {
 }
