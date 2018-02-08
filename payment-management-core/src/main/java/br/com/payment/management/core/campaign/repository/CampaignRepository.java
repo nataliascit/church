67960c84c256708163d146d4b382ba4d22c38fd1
William Custodio <william@pedrinicustodio.com>
2018-02-08 17:59:22 +0000

    [BACKEND] FINALIZING SECURITY MODULE CONFIGURATION.

--- payment-management-core/src/main/java/br/com/payment/management/core/campaign/repository/CampaignRepository.java
+++ payment-management-core/src/main/java/br/com/payment/management/core/campaign/repository/CampaignRepository.java
@@ -1,14 +1,15 @@
 package br.com.payment.management.core.campaign.repository;
 
-import br.com.payment.management.core.campaign.model.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
+
+import br.com.payment.management.core.campaign.model.Campaign;
 
 /**
  * {@link Campaign} Data Access Object.
  *
  * @author wcustodio
  */
-@PreAuthorize("hasRole('ROLE_ADMIN')")
+@PreAuthorize("hasRole('ADMIN')")
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
 }
--- payment-management-core/src/main/java/br/com/payment/management/core/contribution/repository/ContributorRepository.java
+++ payment-management-core/src/main/java/br/com/payment/management/core/contribution/repository/ContributorRepository.java
@@ -1,14 +1,15 @@
 package br.com.payment.management.core.contribution.repository;
 
-import br.com.payment.management.core.contributor.model.Contributor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
+
+import br.com.payment.management.core.contributor.model.Contributor;
 
 /**
  * {@link Contributor} Data Access Object.
  *
  * @author wcustodio
  */
-@PreAuthorize("hasRole('ROLE_ADMIN')")
+@PreAuthorize("hasRole('ADMIN')")
 public interface ContributorRepository extends JpaRepository<Contributor, Long> {
 }
