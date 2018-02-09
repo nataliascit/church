36675fb9d6e353f89daef7ccdb3dafd955bfb6e5
William Custodio <william@pedrinicustodio.com>
2018-02-09 10:27:32 +0000

    [BACKEND] CHANGING SCRIPTS NAME DUE TO ORDER EXECUTION ISSUES.

--- payment-management-webapp/src/main/java/br/com/payment/management/webapp/controller/RoutingController.java
+++ payment-management-webapp/src/main/java/br/com/payment/management/webapp/controller/RoutingController.java
@@ -3,6 +3,11 @@ package br.com.payment.management.webapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller responsible for setting up the templates routing.
+ *
+ * @author wcustodio
+ */
 @Controller
 public class RoutingController {
 
