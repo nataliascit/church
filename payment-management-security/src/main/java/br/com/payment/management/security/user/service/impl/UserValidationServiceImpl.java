325f99f2705bd340ffd8bf132a733a5409c4efdd
William Custodio <william@pedrinicustodio.com>
2018-02-09 14:49:37 +0000

    [BACKEND] CLEANING SOME UNUSED CODE.

--- payment-management-security/src/main/java/br/com/payment/management/security/user/service/impl/UserValidationServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package br.com.payment.management.security.user.service.impl;
-
-import br.com.payment.management.security.user.service.UserValidationService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserValidationServiceImpl implements UserValidationService {
-}
