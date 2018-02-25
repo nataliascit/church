15670f8c88d5195da6decb1938de44f53e4ff8b6
William Custodio <william@pedrinicustodio.com>
2018-02-25 18:57:11 +0000

    [FRONTEND] Defining the menu active bug.

--- payment-management-angular/src/main/resources/static/app/component/menu/navigation/navigationMenuController.js
+++ payment-management-angular/src/main/resources/static/app/component/menu/navigation/navigationMenuController.js
@@ -10,17 +10,20 @@ import '../../../shared/environment/authentication/authenticationRestService';
 
     var paymentManagementModule = angular.module('paymentManagement.menu');
 
-    function NavigationMenuController($window, environmentConfig, authenticationRestService) {
+    function NavigationMenuController($scope, $state, $window, environmentConfig, authenticationRestService) {
 
         var vm = this;
+
+        // Workaround to define the selected menu as active even when its child was selected.
+        $scope.$state = $state;
 
         vm.logoutOnClick = function() {
             authenticationRestService.logout(function() {
                 // Redirects to the login page after successfully logout.
                 $window.location.href = environmentConfig.loginPageUrl;
             });
-        }
+        };
     }
 
-    paymentManagementModule.controller('navigationMenuController', [ '$window', 'environmentConfig', 'authenticationRestService', NavigationMenuController]);
+    paymentManagementModule.controller('navigationMenuController', [ '$scope', '$state', '$window', 'environmentConfig', 'authenticationRestService', NavigationMenuController]);
 }());
