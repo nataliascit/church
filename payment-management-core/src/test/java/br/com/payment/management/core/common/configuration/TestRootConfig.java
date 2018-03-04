f6e3883762e9931d8bc0396d925aa13403afe4ef
William Custodio <william@pedrinicustodio.com>
2018-03-04 20:39:46 +0000

    [BACKEND] Removing some unused annotations.

--- payment-management-core/src/test/java/br/com/payment/management/core/common/configuration/TestRootConfig.java
+++ payment-management-core/src/test/java/br/com/payment/management/core/common/configuration/TestRootConfig.java
@@ -2,8 +2,6 @@ package br.com.payment.management.core.common.configuration;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,9 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  *
  * @author wcustodio
  */
-@Configuration
-@SpringBootApplication
-@ComponentScan(basePackages = {"br.com.payment.management.core"})
+@SpringBootApplication(scanBasePackages = {"br.com.payment.management.core"})
 @EnableJpaRepositories("br.com.payment.management.core")
 @EntityScan(basePackages = {"br.com.payment.management.core"}, basePackageClasses = {Jsr310JpaConverters.class})
 public class TestRootConfig {
--- payment-management-webapp/src/main/java/br/com/payment/management/webapp/Application.java
+++ payment-management-webapp/src/main/java/br/com/payment/management/webapp/Application.java
@@ -3,6 +3,7 @@ package br.com.payment.management.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -18,7 +19,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {
     "br.com.payment.management.core",
     "br.com.payment.management.security"
-})
+}, basePackageClasses = {Jsr310JpaConverters.class})
 @EnableJpaRepositories(basePackages = "br.com.payment.management.core")
 public class Application {
 
